e9e3e9b44cdd5b82ca24ba18ff2ae4d2aff7cce6
bobdawgg <RobBlock@Robert-Blocks-MacBook-Pro-2.local>
2015-08-10 17:52:44 -0700

    Removed UsersFeed XIB & Added UsersFeedCell Xib

---
+++ Run WIth Me User Profile/UsersFeedTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  UsersFeedTableViewCell.swift
+//  Run WIth Me User Profile
+//
+//  Created by Rob Block on 8/10/15.
+//  Copyright (c) 2015 Robert. All rights reserved.
+//
+
+import UIKit
+
+class UsersFeedTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
